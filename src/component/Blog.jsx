30575b3bb489469388ba97106d56bae8db20638e
Apurbo73 <apurbodebnath895@gmail.com>
2023-06-03 03:40:02 +0600

    final push

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import data from "../../src/Data";
 const Blog = () => {
   const { title } = useParams();
-  const [blogData, setData] = useState(" ");
-  useEffect(() => {
-    const detail = data.filter(d => d.title === title);
-    setData(detail[0].description);
-  }, []);
+  const location = useLocation();
+//   console.log(location)
+//   const [blogData, setData] = useState(" ");
+//   useEffect(() => {
+//     const detail = data.filter(d => d.title === title);
+//     setData(detail[0].description);
+//   }, []);
   return (
     <div className="container mx-auto">
       <div className="card m-3" style={{ width: "18rem" }}>
         <div className="card-body">
-          <h5 className="card-title">
+          <h5 className="card-title text-center">
             {title}
           </h5>
           <p style={{ textAlign: "justify" }} className="card-text ">
-            {blogData}
+            {location.state.description}
           </p>
+          <img src={location.state.image} className="card-img-top" alt="..." />
+
         </div>
       </div>
       <Link to="/blogs" className="btn btn-primary w-25 ">
--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -10,20 +10,22 @@ const Blogs = () => {
 
       <div className="d-flex flex-wrap">
         {dataBlogs.map(d => {
+          const { id, image, title, description } = d;
           return (
             <div className="card m-3" style={{ width: "18rem" }}>
               <img src={d.image} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">
-                  {d.title}
+                  {title}
                 </h5>
                 <p className="card-text">
-                  Id: {d.id}
+                  Id: {id}
                 </p>
-                <p style={{textAlign:"justify"}} className="card-text ">
-                  Id: {d.description}
-                </p>
-                <Link to={d.title} className="btn btn-primary w-100">
+                <Link
+                  to={d.title}
+                  state={{ image, description, id }}
+                  className="btn btn-primary w-100"
+                >
                   See More...
                 </Link>
               </div>
